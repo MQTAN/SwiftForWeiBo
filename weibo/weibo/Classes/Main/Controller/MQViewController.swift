1016bedc6fa63b7d8a51e3ff4fc5b7434e69da7b
tanmanqi <tanmanqi@tanmanqi.local>
2015-11-21 12:57:01 +0800

    创建零时的控制器测试用.

---
+++ weibo/weibo/Classes/Main/Controller/MQViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MQViewController.swift
+//  weibo
+//
+//  Created by tanmanqi on 15/11/21.
+//  Copyright © 2015年 tanmanqi. All rights reserved.
+//
+
+import UIKit
+
+class MQViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
